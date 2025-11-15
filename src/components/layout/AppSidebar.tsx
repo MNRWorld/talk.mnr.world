71ab3d65f5adc898c6c5eb437051e5d4ed3f00b4
MNR from 2020 <contact.mnrfrom2020@gmail.com>
2025-11-15 18:47:29 +0000

    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 192 171.000002" pre

--- src/components/layout/AppSidebar.tsx
+++ src/components/layout/AppSidebar.tsx
@@ -5,7 +5,6 @@ import {
   Home,
   Library,
   Plus,
-  Podcast as PodcastIcon,
   Search,
   Grid,
 } from "lucide-react";
@@ -28,7 +27,21 @@ export default function AppSidebar() {
     <Sidebar>
       <SidebarHeader>
         <div className="flex items-center gap-2 p-2">
-          <PodcastIcon className="text-primary w-8 h-8" />
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 192 171.000002"
+            preserveAspectRatio="xMidYMid meet"
+            className="w-8 h-8"
+          >
+            <path
+              fill="#f41212"
+              d="M149.06 94.19 76.86 94.36l14.84 25.57 98.12-.22-68.58-118.16-19.41 11.26 47.23 81.38Z"
+            />
+            <path
+              fill="#009f0b"
+              d="m2.14 50.25 68.58 118.16 19.42-11.26-47.23-81.41 72.2-0.16-14.84-25.57-98.12.22Z"
+            />
+          </svg>
           <h1 className="text-xl font-bold font-headline">PodLink</h1>
         </div>
       </SidebarHeader>
